16373171ea52c34b2b6ec7c0b81ed476b87dd401
Davide Prencipe <davidedev04@gmail.com>
2023-11-16 23:47:00 +0100

    esercizio terminato

--- password.js
+++ password.js
@@ -3,3 +3,20 @@
 
 // chiedi all'utente il suo nome
 
+const userName = prompt ("inserisci il tuo nome");
+
+// chiedi il suo cognome
+
+const userSurname = prompt ("inserisci il tuo cognome");
+
+// chiedi il suo colore preferito
+
+const userColor = prompt ("inserisci il tuo colore preferito");
+
+// codice sulla pagina e sulla console
+
+let password = `Questa è la tua password ${userName}${userSurname}${userColor}23`;
+
+document.getElementById("my_Password") .innerHTML = password;
+
+console.log (password);
